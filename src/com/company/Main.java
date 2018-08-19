51f312d53977d19025987c416f8706e127f86057
maxresh <maxim_reshetnikov@mail.ru>
2018-08-19 15:13:30 +0200

    added random number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Random random = new Random();
+        int liczba = random.nextInt(100);
+        System.out.println(liczba);
     }
 }
